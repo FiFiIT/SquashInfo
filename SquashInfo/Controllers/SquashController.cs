c9386bec950afb22886c1154f4bac2a79d87b20d
FiFiIT <filip.tyborowski@gmail.com>
2019-09-04 22:28:38 +0200

    Add project files.

---
+++ SquashInfo/Controllers/SquashController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SquashInfo.Controllers
+{
+    [Route("squash")]
+    public class SquashController : Controller
+    {
+        [HttpGet("freeCourts")]
+        public JsonResult GetCourts()
+        {
+            return new JsonResult(SquashDataStore.Current.FreeCourts);
+        }
+
+        [HttpGet("freeCourts/{from}/{to}")]
+        public IActionResult GetFreeCourtsFromTo(string from, string to)
+        {
+
+            if(!DateTime.TryParse(from, out DateTime fromTime))
+            {
+                return BadRequest($"System was not able to parse {from} to DateTime.");
+            }
+
+            if (!DateTime.TryParse(to, out DateTime toTime))
+            {
+                return BadRequest($"System was not able to parse {to} to DateTime.");
+            }
+
+            var result = SquashDataStore.Current.FreeCourts.Where(c => c.From >= fromTime && c.To <= toTime);
+            if(result == null || result.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ SquashInfo/SquashDataStore.cs
@@ -0,0 +1,27 @@
+﻿using SquashInfo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SquashInfo
+{
+    public class SquashDataStore
+    {
+        public static SquashDataStore Current { get; } = new SquashDataStore();
+        public List<CourtDto> FreeCourts { get; set; }
+
+        public SquashDataStore()
+        {
+            FreeCourts = new List<CourtDto>
+            {
+                new CourtDto(){Number=1, From = new DateTime(2019,9,4,19,0,0), To= new DateTime(2019,9,4,19,30,0)},
+                new CourtDto(){Number=1, From = new DateTime(2019,9,4,19,30,0), To= new DateTime(2019,9,4,20,00,0)},
+                new CourtDto(){Number=3, From = new DateTime(2019,9,4,17,0,0), To= new DateTime(2019,9,4,17,30,0)},
+                new CourtDto(){Number=3, From = new DateTime(2019,9,4,17,30,0), To= new DateTime(2019,9,4,18,00,0)},
+                new CourtDto(){Number=10, From = new DateTime(2019,9,4,16,0,0), To= new DateTime(2019,9,4,16,30,0)},
+                new CourtDto(){Number=10, From = new DateTime(2019,9,4,17,0,0), To= new DateTime(2019,9,4,17,30,0)}
+            };
+        }
+    }
+}
